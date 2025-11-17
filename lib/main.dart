9799028532f5119f40977ff59966be37d664e76d
Goff120 <geoffreysmithuk@icloud.com>
2025-11-17 16:50:23 +0000

    add styles footer file with 3 types of test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
+import 'package:union_shop/styles/footer_style.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -290,18 +291,14 @@ class HomeScreen extends StatelessWidget {
 
             // Footer
             Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+                width: double.infinity,
+                color: Colors.grey[50],
+                padding: const EdgeInsets.all(24),
+                child: const Column(
+                  children: <Widget>[
+                    
+                  ],
+                )),
           ],
         ),
       ),
---
+++ lib/styles/footer_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const TextStyle footNormal = TextStyle(
+  color: Colors.grey,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle footHeader2 = TextStyle(
+  color: Color.fromARGB(255, 97, 97, 97),
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle footHeader1 = TextStyle(
+  color: Color.fromARGB(255, 97, 97, 97),
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+
+
