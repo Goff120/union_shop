7163944f710811dcfdb67c2e6fcf080dcc77568d
Goff120 <geoffreysmithuk@icloud.com>
2025-11-28 18:36:18 +0000

    button making class

---
+++ lib/part_builder/styled_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/styles/genral_text.dart';
+
+class StyledButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final IconData icon;
+  final String label;
+  final Color backgroundColor;
+
+  const StyledButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+    required this.label,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle myButtonStyle = ElevatedButton.styleFrom(
+      backgroundColor: backgroundColor,
+      foregroundColor: Colors.white,
+      textStyle: genNormal,
+    );
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: myButtonStyle,
+      child: Row(children: [Icon(icon), const SizedBox(width: 8), Text(label)]),
+    );
+  }
+}
