3e26cf7301b4d6772182b82c60d78b142fff16f8
Goff120 <geoffreysmithuk@icloud.com>
2025-12-02 19:58:09 +0000

    a just numbers to size

--- lib/styles/cart_style.dart
+++ lib/styles/cart_style.dart
@@ -2,19 +2,15 @@ import 'package:flutter/material.dart';
 
 const TextStyle cartNormal = TextStyle(
   color: Color.fromARGB(255, 65, 65, 65),
-  fontSize: 8,
-  fontWeight: FontWeight.w600,
-);
-
-const TextStyle cartHeader2 = TextStyle(
-  color: Color.fromARGB(255, 38, 38, 38),
-  fontSize: 10,
+  fontSize: 12,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle cartHeader1 = TextStyle(
-  color: Color.fromARGB(255, 0, 0, 0),
-  fontSize: 12,
+  color: Color.fromARGB(255, 38, 38, 38),
+  fontSize: 15,
   fontWeight: FontWeight.w600,
 );
+
+
 
