b694cd76d6c208a34709e81eb3c67f5b2dcbba1e
Goff120 <geoffreysmithuk@icloud.com>
2025-12-02 16:09:26 +0000

    add cart and adder

---
+++ lib/logic/cart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String imageUrl;
+  final String name;
+  final String price;
+  int quantity;
+
+  CartItem({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
+
+}
+class Cart extends ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  void addItem(String id, String imageUrl, String name, String price, int quantity) {
+    if (_items.containsKey(id)) {
+      _items[id]!.quantity += quantity;
+    } else {
+      _items[id] = CartItem(
+        imageUrl: imageUrl,
+        name: name,
+        price: price,
+        quantity: quantity,
+      );
+    }
+    notifyListeners(); //re build page
+  }
+}
