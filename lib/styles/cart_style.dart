149d764845c042563d291a69f8268662310123b0
Goff120 <geoffreysmithuk@icloud.com>
2025-12-02 19:49:59 +0000

    new cart styles

---
+++ lib/styles/cart_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const TextStyle cartNormal = TextStyle(
+  color: Color.fromARGB(255, 65, 65, 65),
+  fontSize: 8,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle cartHeader2 = TextStyle(
+  color: Color.fromARGB(255, 38, 38, 38),
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle cartHeader1 = TextStyle(
+  color: Color.fromARGB(255, 0, 0, 0),
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
