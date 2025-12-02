65f5eb37f42757c4eb5ec0268af1ae07b2a1e275
Goff120 <geoffreysmithuk@icloud.com>
2025-12-02 15:18:36 +0000

    remove

--- lib/logic/item_json.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
-
-//load json
-Future<Map<String, dynamic>> loadItems() async {
-  final jsonString = await rootBundle.loadString('assets/json/items.json');
-  return jsonDecode(jsonString) as Map<String, dynamic>;
-}
